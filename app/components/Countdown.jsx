418104568aed0339cf783d7ceacad2d2016d2a74
Michael James <mjames@atp.com>
2018-05-18 13:57:53 -0500

    updated clock component and clock styles

--- app/components/Countdown.jsx
+++ app/components/Countdown.jsx
@@ -1,8 +1,13 @@
 var React = require('react');
+var Clock = require('Clock');
 
 var Countdown = React.createClass({
   render: function () {
-    return <h3>Countdown Component</h3>
+    return (
+      <div>
+        <Clock totalSeconds={ 129 }/>
+      </div>
+    );
   }
 });
 
